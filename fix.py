3ea33910de6de20376b6d55c121fdad4c0b58160
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-09-23 12:59:19 -0500

    final tweaks

--- fix.py
+++ fix.py
@@ -1,14 +1,14 @@
 import cadquery as cq
 
 
-height = 10
+height = 17
 pts = [
     (0, 0),
-    (0, 8),
-    (2,8),
+    (0,5),
+    (2,5),
     (2,2),
-    (6,2),
-    (6,0),
+    (4.2,2),
+    (4.2,0),
 ]
 holder_radius = 2.7/2
 l = cq.Workplane("front").polyline(pts).close().extrude(height)
