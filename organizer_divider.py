87c65f13cd58fec6be1130f7d6d1f2d2aced63ad
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-11-10 18:55:46 -0500

    git push

---
+++ organizer_divider.py
@@ -0,0 +1,14 @@
+import cadquery as cq
+
+height = 30
+pts = [
+    (0, 0),
+    (0,140),
+    (4,140),
+    (4,72),
+    (70,72),
+    (70,68),
+    (4, 68),
+    (4,0),
+]
+l = cq.Workplane("front").polyline(pts).close().extrude(height)
