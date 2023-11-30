1e2643c8dbcc81a68cff4be50c7de39ca8218c5a
Cristopher Pinzon <cristopher.pinzon@gmail.com>
2023-11-30 18:57:33 -0500

    silverware organizer

---
+++ silverwear_organizer.py
@@ -0,0 +1,22 @@
+import cadquery as cq
+
+limit_height = 50 # mm
+limit_width = 200 # mm
+limit_depth = 150 # mm
+base_height = 6 # mm
+barrier_width = 4 # mm
+
+
+def make_base():
+    return cq.Workplane("front").box(limit_depth,limit_width, limit_height).faces("+Z").shell(base_height, "intersection")
+
+
+def make_barriers():
+    return cq.Workplane("front").box(barrier_width,limit_width, limit_height)
+
+
+def make_organizer():
+    return make_base().union(make_barriers())
+
+
+result = make_organizer()
